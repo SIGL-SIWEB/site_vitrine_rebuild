234847c4b16c58410f4d396a743058824d84261f
Dylan Chaperon <dylan.chaperon@epita.fr>
2024-05-22 08:11:21 +0200

    [FEAT] add error if not filled

--- src/app/contact/ContactForm.tsx
+++ src/app/contact/ContactForm.tsx
@@ -12,18 +12,39 @@ export function ContactForm() {
       email: '',
       message: ''
   });
+
+  const [errors, setErrors] = useState({
+    name: false,
+    email: false,
+    message: false
+  });
   
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       setFormData({
         ...formData,
         [e.target.name]: e.target.value
       });
+      setErrors({
+        ...errors,
+        [e.target.name]: false
+      });
   };
 
   const handleSubmit = (e: React.FormEvent) => {
       e.preventDefault();
-      const mailtoLink = `mailto:${emailToSend}?subject=[FAQ][SIGL] Question du site vitrine&body=Bonjour,%0D%0A%0D%0AL'étudiant ${formData.name} (${formData.email}) vous a envoyé cette question via la FAQ du site vitrine :%0D%0A%0D%0A${formData.message}%0D%0A%0D%0ACordialement,%0D%0A%0D%0A-- %0D%0AFAQ site vitrine SIGL`;
-      window.location.href = mailtoLink;
+
+      const newErrors = {
+        name: formData.name === '',
+        email: formData.email === '',
+        message: formData.message === ''
+      };
+  
+      if (newErrors.name || newErrors.email || newErrors.message) {
+        setErrors(newErrors);
+      } else {
+        const mailtoLink = `mailto:${emailToSend}?subject=[FAQ][SIGL] Question du site vitrine&body=Bonjour,%0D%0A%0D%0AL'étudiant ${formData.name} (${formData.email}) vous a envoyé cette question via la FAQ du site vitrine :%0D%0A%0D%0A${formData.message}%0D%0A%0D%0ACordialement,%0D%0A%0D%0A-- %0D%0AFAQ site vitrine SIGL`;
+        window.location.href = mailtoLink;
+      }
   };
   
  
@@ -31,16 +52,18 @@ export function ContactForm() {
     <FadeIn className="lg:order-last">
       <form onSubmit={handleSubmit}>
         <div className="isolate mt-6 -space-y-px rounded-2xl bg-white/50">
-          <TextInput label="Name" name="name" autoComplete="name" value={formData.name} onChange={handleChange}/>
+          <TextInput label="Name" name="name" autoComplete="name" value={formData.name} onChange={handleChange} error={errors.name}/>
           <TextInput
             label="Email"
             type="email"
             name="email"
             autoComplete="email"
+            error={errors.email}
             value={formData.email}
             onChange={handleChange}
+            className={errors.email ? 'border-red-500' : ''}
           />
-          <TextInput label="Question" name="message" value={formData.message} onChange={handleChange}/>
+          <TextInput label="Question" name="message" value={formData.message} onChange={handleChange} error={errors.message}/>
         </div>
         <Button type="submit" className="mt-10">
           Submit your question !
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,7 +9,7 @@ export default function Contact() {
         <p></p>
       </PageIntro>
 
-      <Container className="mt-24 sm:mt-32 lg:mt-40">
+      <Container className="mt-12 sm:mt-16 lg:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-24 lg:grid-cols-2">
           <ContactForm />
         </div>
