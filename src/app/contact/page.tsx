a87f04e067311ffc704a518211c955108e0cc62f
Dylan Chaperon <dylan.chaperon@epita.fr>
2024-05-26 20:11:17 +0200

    [FIX] remove useless space

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,6 @@ export default function Contact() {
       <PageIntro eyebrow="Contact us" title="Question / Answer">
         <p></p>
       </PageIntro>
-
       <Container className="mt-12 sm:mt-16 lg:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-24 lg:grid-cols-2">
           <ContactForm />
