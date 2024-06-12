332c2c33e01ef099a2ace202e76046022bd39831
Dylan Chaperon <dylan.chaperon@epita.fr>
2024-06-12 20:34:13 +0200

    [FIX] use navigator language

--- src/app/components/RootLayout.tsx
+++ src/app/components/RootLayout.tsx
@@ -17,6 +17,7 @@ import { NavBar } from '@/app/components/NavBar'
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 const RootLayoutContext = createContext<{
   logoHovered: boolean
@@ -84,6 +85,7 @@ export function RootLayout({ children }: { children: React.ReactNode }) {
   i18n
   .use(HttpBackend)
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
     lng: 'fr',
     fallbackLng: 'en',
