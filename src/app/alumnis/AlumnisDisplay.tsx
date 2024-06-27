a58dc70635565aff239f83f8a5b74ed2e31227b6
Dylan Chaperon <dylan.chaperon@epita.fr>
2024-06-27 23:53:28 +0200

    [FIX] translation bug

--- src/app/alumnis/AlumnisDisplay.tsx
+++ src/app/alumnis/AlumnisDisplay.tsx
@@ -4,8 +4,8 @@ import { DelegateInfo } from '@/app/alumnis/DelegateInfo'
 
 export function AlumnisDisplay({alumnis}: {alumnis: { image: StaticImageData, title: string, names: string[], linkedinUrl: string[], year: number }[]}) {
   return (
-    alumnis.map((alumni) => (
-        <Promotion title={alumni.title} image={alumni.image}>
+    alumnis.map((alumni, index) => (
+        <Promotion title={alumni.title} image={alumni.image} key={index}>
           <DelegateInfo delegates={ { names: alumni.names, linkedinUrl: alumni.linkedinUrl, year: alumni.year } }></DelegateInfo>
         </Promotion>
     ))
